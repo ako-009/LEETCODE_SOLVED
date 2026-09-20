8ffeb50bd540d9e90df137fd820a81c5e8a8f6ad
Abhishek Kumar Ojha <ojhaabhishekkumar009@gmail.com>
2026-09-20 06:44:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (34.96%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -5,14 +5,6 @@ public:
         int xi = clamp(xCenter, x1, x2);
         int yi = clamp(yCenter, y1, y2);
 
-        if(y1 > yCenter) {
-            yi = y1;
-        } else if(y2 < yCenter) {
-            yi = y2;
-        } else {
-            yi = yCenter;
-        }
-
         //(xi, yi) ------- (xCenter, yCenter)
         return sqrt((xi - xCenter)*(xi - xCenter) + (yi - yCenter)*(yi - yCenter)) <= radius;
     }
